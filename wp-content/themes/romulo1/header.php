76a07934fe488b00c54f21738d423dfccfa2eae0
Romulo Pransteter <r.pransteter@gmail.com>
2016-02-16 23:18:38 -0200

    slidetoggle ok

--- wp-content/themes/romulo1/header.php
+++ wp-content/themes/romulo1/header.php
@@ -16,9 +16,9 @@
                     <img src="<?php echo get_template_directory_uri(); ?>/imgs/logo-demo.jpg">
                 </div>
                 <div class="box-btn-menu">
-                    <img class="btn-menu-mobile" src="<?php echo get_template_directory_uri(); ?>/imgs/mobile-icon.svg">
+                    <img id="btn-mobile" class="btn-menu-mobile" src="<?php echo get_template_directory_uri(); ?>/imgs/mobile-icon.svg">
                 </div>
             </div>
-            <?php wp_nav_menu(array('container' => '')); ?>
+            <?php wp_nav_menu(array('container' => '' , 'menu_id' => 'menu-principal')); ?>
         </header>
         <section>
