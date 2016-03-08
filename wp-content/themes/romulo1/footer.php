6e0f3de58210ffe547764fda4406809f70a289fc
Romulo Pransteter <r.pransteter@gmail.com>
2016-03-08 02:20:35 -0300

    comeco da pagina inicial, redefinicao do header para combinar com slider, ficou lindo

--- wp-content/themes/romulo1/footer.php
+++ wp-content/themes/romulo1/footer.php
@@ -1,7 +1,7 @@
             </section>
             <footer id="bottom">
 				<div class="container-desenvolvido"><p>Desenvolvido por: <em>Rômulo Pransteter da Silva</em></p></div>
-				<div class="container-contato"><p>Contato</p><!--Contato - Possível botão para abrir modal --></div>
+				<div class="container-contato"><p></p><!--Contato - Possível botão para abrir modal --></div>
 				<div class="container-icons">
 					<div class="component">
 						<a href="#" class="icon icon-cube facebook">facebook</a>
@@ -11,12 +11,11 @@
 				</div>
             </footer>
         </div>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/bower-components/components/jquery/dist/jquery.min.js"></script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/bower-components/components/jquery-ui/ui/core.js"></script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/bower-components/components/jquery-ui/ui/effect.js"></script>
+		<!--<script type="text/javascript" src="<?php /*echo get_template_directory_uri(); */?>/assets/bower-components/components/jquery-ui/ui/core.js"></script>
+		<script type="text/javascript" src="<?php /*echo get_template_directory_uri(); */?>/assets/bower-components/components/jquery-ui/ui/effect.js"></script>-->
 		<script type="text/javascript" >
 			var URL_MAIN = '<?php echo get_template_directory_uri(); ?>';
 		</script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/main.js"></script>
+		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/scripts/main.js"></script>
 	</body>
 </html>
---
+++ wp-content/themes/romulo1/scripts/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mikau on 08/03/16.
+ */
+var slider = document.getElementById('slider-principal');
+
+$(slider).flexslider();
